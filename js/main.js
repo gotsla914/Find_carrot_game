030b9a800dc94f39ec839d001022c3cbaec8b009
ddori <gotsla914@naver.com>
2022-03-03 13:05:35 +0900

    time over modal & time counter

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const modal = document.querySelector('.modal');
-const madalOverlay = document.querySelector('.madalOverlay');
+const modalOverlay = document.querySelector('.madalOverlay');
 const btnClose = document.querySelector('.btnClose');
 
 
@@ -7,12 +7,37 @@ const openModal = () => {
     modal.classList.remove('objHidden');
 }
 
-const btnStart =document.querySelector('.btnStart');
+const btnStart = document.querySelector('.btnStart');
 btnStart.addEventListener('click', openModal);
 
 const closeModal = () => {
     modal.classList.add('objHidden');
-    console.log(modal);
 }
 btnClose.addEventListener('click', closeModal);
-madalOverlay.addEventListener('click', closeModal);
+modalOverlay.addEventListener('click', closeModal);
+
+
+
+// 시간초과
+const gameOver = document.querySelector('.gameOver');
+const btnGameOverClose = document.querySelector('.gameOverClose');
+const gameOverlay = document.querySelector('.gameOverlay')
+
+const openTimOutModal = () => {
+    gameOver.classList.remove('objHidden');
+}
+setTimeout(openTimOutModal, 3000);
+
+const closeGameOverModal = () => {
+    gameOver.classList.add('objHidden');
+}
+btnGameOverClose.addEventListener('click', closeGameOverModal);
+
+const timer = document.querySelector('.timer');
+let timeCount = 0;
+setInterval(updateTimeCount, 1000);
+
+function updateTimeCount() {
+    timeCount++;
+    timer.innerHTML = timeCount;
+}
